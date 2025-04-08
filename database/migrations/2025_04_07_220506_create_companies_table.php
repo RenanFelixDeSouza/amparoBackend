64b96d16b4e912da6a0fc7b925a4f1cdc68254d1
RenanFelixDeSouza <renan050900@gmail.com>
2025-04-07 22:14:29 -0400

    Adiciona tabelas de empresas e de relacionamento entre empresas e usuários

---
+++ database/migrations/2025_04_07_220506_create_companies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->string('fantasy_name');
+            $table->string('cnpj', 14)->unique();
+            $table->string('ie')->nullable();
+            $table->string('im')->nullable();
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->string('responsible_name')->nullable();
+            $table->foreignId('address_id')->constrained('addresses')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
